65f2545a7869ba8580fb62baa402ccdb5f5d6ba6
guusfelix2015 <gusfelix2015@gmail.com>
2024-04-26 17:36:58 -0300

    feat: MUI setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import "./App.css";
+import { Box, Button, Container, Typography } from "@mui/material";
 
 function App() {
   return (
-    <>
-      <h1>Opa</h1>
-    </>
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+          Material UI Create React App example with styled-components in
+          TypeScript
+        </Typography>
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Box>
+    </Container>
   );
 }
 
