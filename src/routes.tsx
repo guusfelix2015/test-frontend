69670f38f88045cda0bb081b5fe5a23a67334d30
guusfelix2015 <gusfelix2015@gmail.com>
2024-04-28 17:38:46 -0300

    feat: create route edit cutomer

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Home } from "./pages/home/home";
 import { CreateCustomer } from "./pages/create/create";
+import { EditCustomer } from "./pages/edit/edit";
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ export const router = createBrowserRouter([
   {
     path: "/create",
     element: <CreateCustomer />,
+  },
+  {
+    path: "/edit/:id",
+    element: <EditCustomer />,
   },
 
   {
