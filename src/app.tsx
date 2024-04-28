dafb34b7de0a8a210f37fd34034ec13e6d648ea7
guusfelix2015 <gusfelix2015@gmail.com>
2024-04-28 12:41:25 -0300

    refactor: import queryClient

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,8 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 
-import { queryClient } from "./lib/react-query";
 import { router } from "./routes";
+import { queryClient } from "./shared/lib/react-query";
 
 export function App() {
   return (
