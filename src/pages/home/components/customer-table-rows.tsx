ddd8daf5708e5d83bdf7a55155b456e85fa7fd59
guusfelix2015 <gusfelix2015@gmail.com>
2024-04-28 16:13:16 -0300

    feat: create modal delete customer

--- src/pages/home/components/customer-table-rows.tsx
+++ src/pages/home/components/customer-table-rows.tsx
@@ -1,34 +1,111 @@
-import { TableCell, TableRow, IconButton } from "@mui/material";
+import {
+  TableCell,
+  TableRow,
+  IconButton,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+} from "@mui/material";
 import EditRoundedIcon from "@mui/icons-material/EditRounded";
 import DeleteRoundedIcon from "@mui/icons-material/DeleteRounded";
 import { Customer } from "../../../shared/interfaces";
+import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { CustomerService } from "../../../shared/services";
+import { queryClient } from "../../../shared/lib/react-query";
 
 export interface Props {
   customer: Customer;
 }
 
 export const CustomerTableRows = ({ customer }: Props) => {
+  const [open, setOpen] = useState(false);
+  const [selectedCustomerId, setSelectedCustomerId] = useState<string | null>(
+    null
+  );
+
+  const handleClickOpen = (customerId: string) => {
+    setSelectedCustomerId(customerId);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  console.log(selectedCustomerId);
+
+  const { mutate } = useMutation({
+    mutationFn: (customerId: string) =>
+      CustomerService.deleteCustomer(customerId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["customers"] });
+      handleClose();
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+  });
+
   return (
-    <TableRow>
-      <TableCell>{customer.type}</TableCell>
-      <TableCell>
-        {customer.name
-          ? customer.name
-          : customer.companyName && customer.tradeName
-          ? `${customer.companyName} (${customer.tradeName})`
-          : customer.name}
-      </TableCell>
-      <TableCell>{customer.document}</TableCell>
-      <TableCell>{customer.email}</TableCell>
-      <TableCell>{customer.phoneNumber}</TableCell>
-      <TableCell>
-        <IconButton size="small">
-          <EditRoundedIcon fontSize="small" />
-        </IconButton>
-        <IconButton size="small">
-          <DeleteRoundedIcon fontSize="small" />
-        </IconButton>
-      </TableCell>
-    </TableRow>
+    <>
+      <TableRow>
+        <TableCell>{customer.type}</TableCell>
+        <TableCell>
+          {customer.name
+            ? customer.name
+            : customer.companyName && customer.tradeName
+            ? `${customer.companyName} (${customer.tradeName})`
+            : customer.name}
+        </TableCell>
+        <TableCell>{customer.document}</TableCell>
+        <TableCell>{customer.email}</TableCell>
+        <TableCell>{customer.phoneNumber}</TableCell>
+        <TableCell>
+          <IconButton size="small">
+            <EditRoundedIcon fontSize="small" />
+          </IconButton>
+          <IconButton
+            size="small"
+            onClick={() => {
+              handleClickOpen(customer.id);
+            }}
+          >
+            <DeleteRoundedIcon fontSize="small" />
+          </IconButton>
+        </TableCell>
+      </TableRow>
+
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Delete Customer?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {customer.id}
+            Are you sure you want to delete the client?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Disagree</Button>
+          <Button
+            onClick={() => {
+              if (selectedCustomerId) {
+                mutate(selectedCustomerId);
+              }
+            }}
+            autoFocus
+          >
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
